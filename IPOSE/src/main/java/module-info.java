b6f2266ef61074f02285c09d3cd9a3ab0265af7b
AHsofty <e.basaran2004@gmail.com>
2023-04-03 12:33:22 +0200

    De plaatjes laden gefixt

--- IPOSE/src/main/java/module-info.java
+++ IPOSE/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.ipose {
     requires javafx.fxml;
 
     requires com.almasb.fxgl.all;
+    opens assets.textures;
 
     opens com.example.ipose to javafx.fxml;
     exports com.example.ipose;
