817d47757a59e4d2ea98dbd7c5709a2d103eb0c3
AHsofty <e.basaran2004@gmail.com>
2023-04-06 10:09:01 +0200

    Score systeem

--- IPOSE/src/main/java/com/example/ipose/Main.java
+++ IPOSE/src/main/java/com/example/ipose/Main.java
@@ -53,16 +53,25 @@ public class Main extends GameApplication {
     private void gameEnd(boolean reachedEndOfGame) {
         StringBuilder builder = new StringBuilder();
         if (reachedEndOfGame) {
-            builder.append("You have reached the end of the game!\n\n");
             FileManager FM = new FileManager();
             FM.setMaxLevel(this.userName, this.level + 1);
-        }else{
+
+            int score = FXGL.geti("score");
+            FM.setNewScore(this.userName, this.level, String.valueOf(score));
+            int highestScore = FM.getHighScore(this.level);
+            builder.append("You have reached the end of the game!\n\nHighest score was: ")
+                    .append(highestScore)
+                    .append("\n\n");
+
+        }
+        else{
             builder.append("Game Over!\n\n");
         }
         builder.append("Final score: ")
                 .append(FXGL.geti("score"))
                 .append("\nFinal level: ")
                 .append(this.level);
+
 
         FXGL.getDialogService().showMessageBox(builder.toString(), this::LevelScreen);
     }
